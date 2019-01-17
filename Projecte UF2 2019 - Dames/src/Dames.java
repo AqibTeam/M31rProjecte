e4521c2a49e66ea8047d5836e3b40d5aa619a686
SrWhite23 <sergiocraft23@gmail.com>
2019-01-17 16:09:09 +0100

    Update Dames.java

--- Projecte UF2 2019 - Dames/src/Dames.java
+++ Projecte UF2 2019 - Dames/src/Dames.java
@@ -1,4 +1,41 @@
 
 public class Dames {
 
+	static void omplirTauler(String tablero[][], int blanconegre) {
+
+		for (int i = 0; i < tablero.length; i++) {
+			for (int j = 0; j < tablero.length; j++) {
+				blanconegre++;
+				if ((blanconegre % 2) == 0) {
+					tablero[i][j] = "██";
+				} else {
+					tablero[i][j] = "  ";
+				}
+			}
+			blanconegre++;
+		}
+	}
+
+	static void pintaTauler(String tablero[][]) {
+
+		for (int i = 0; i < tablero.length; i++) {
+			for (int j = 0; j < tablero.length; j++) {
+				System.out.print(tablero[i][j]);
+			}
+			System.out.println();
+		}
+	}
+
+	
+	public static void main(String[] args) {
+
+		String[][] tablero = new String[9][9];
+		int blanconegre = 0;
+
+		omplirTauler(tablero, blanconegre);
+		
+		pintaTauler(tablero);
+		
+	}
+
 }
