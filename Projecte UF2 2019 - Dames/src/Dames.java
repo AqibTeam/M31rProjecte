73018a77f268dae1d1ae47d61986041aa9138d26
SrWhite23 <sergiocraft23@gmail.com>
2019-03-07 09:19:38 +0100

    final

--- Projecte UF2 2019 - Dames/src/Dames.java
+++ Projecte UF2 2019 - Dames/src/Dames.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class Dames {
 
+
+
+
 	static void omplirTauler(String tablero[][], int blanconegre) {
 
 		for (int i = 0; i < tablero.length; i++) {
@@ -18,6 +21,8 @@ public class Dames {
 		}
 	}
 
+
+
 	static void pintaTauler(String tablero[][]) {
 		
 		for (int i = 0; i < tablero.length; i++) {
@@ -27,25 +32,27 @@ public class Dames {
 			}
 			System.out.println();
 		}
-		System.out.println("    |A||B||C||D||E||F||G||H||I| ");
+		System.out.println("    |A||B||C||D||E||F||G||H| ");
 	}
+
+
 	
 	static void afegirFitxes(String tablero[][], int blanconegre) {
 		//blanques
 		
 		blanconegre=0;
-		for (int i = 0; i < 2; i++) {
+		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < tablero.length; j++) {
 				
-				if (blanconegre==0) {
+				if (blanconegre % 2 ==0) {
 					if ((j % 2) != 0) {
 						tablero[i][j] = "█◘█";
 					} else {
-						tablero[i][j] = " ○ ";
+						tablero[i][j] = "   ";
 					}
 				}else {
 					if ((j % 2) != 0) {
-						tablero[i][j] = " ○ ";
+						tablero[i][j] = "   ";
 					} else {
 						tablero[i][j] = "█◘█";
 					}
@@ -59,16 +66,15 @@ public class Dames {
 		
 		for (int i = tablero.length-1; i > (tablero.length-4); i--) {
 			for (int j = 0; j < tablero.length; j++) {
-				
-				if (blanconegre==0) {
+				if (blanconegre%2 !=0) {
 					if ((j % 2) != 0) {
 						tablero[i][j] = "█◙█";
 					} else {
-						tablero[i][j] = " • ";
+						tablero[i][j] = "   ";
 					}
 				}else {
 					if ((j % 2) != 0) {
-						tablero[i][j] = " • ";
+						tablero[i][j] = "   ";
 					} else {
 						tablero[i][j] = "█◙█";
 					}
@@ -77,9 +83,299 @@ public class Dames {
 			blanconegre++;
 		}
 	}
+	
+	
+
+
+	static int seleccionarfitxan(String tablero[][], int fila, String columna ) {
+		
+		int columnaparse;
+		int accio = 0;
+		Scanner teclado = new Scanner(System.in);
+
+		switch(columna) {
+			case "A": columnaparse=0;
+				break;
+			case "B": columnaparse=1;
+				break;
+			case "C": columnaparse=2;
+				break;
+			case "D": columnaparse=3;
+				break;
+			case "E": columnaparse=4;
+				break;
+			case "F": columnaparse=5;
+				break;
+			case "G": columnaparse=6;
+				break;
+			case "H": columnaparse=7;
+				break;
+			default:
+				System.out.println("Posicio incorrecta, torna a escollir:");
+				columnaparse=666;
+		}
+
+		int filamovida = fila;
+		int columnamovida = columnaparse;
+		int retorn = 0;
+do{
+		if (columnaparse != 666 && (fila <= 7 && fila >= 0)){
+			if(tablero[fila][columnaparse]=="█◙█"){
+
+				
+					filamovida=fila;
+					columnamovida = columnaparse;
+				System.out.println("Cap a on vols avançar?");
+				System.out.println("╔═══════════════════════╗");
+				System.out.println("║1. Avançar dreta       ║");
+				System.out.println("║2. Avançar esquerra    ║");				
+				System.out.println("║3. Enrere              ║");
+				System.out.println("╚═══════════════════════╝");
+				
+				accio = teclado.nextInt();
+
+				switch(accio) {
+					case 1: 
+						filamovida--;
+						columnamovida++;
+						break;
+					case 2: 
+						filamovida--;
+						columnamovida--;
+						break;
+					case 3:
+						filamovida = 666;
+						columnamovida = 666;
+					default:
+						accio=666;
+						break;
+
+					}
+
+
+					if((filamovida <= 7 && filamovida >= 0) && (columnamovida <= 7 && columnamovida >= 0)){
+						if (tablero[filamovida][columnamovida] != "█◙█"){
+							if(tablero[filamovida][columnamovida] == "█◘█"){
+								int filamovida2 = filamovida;
+								int columnamovida2 = columnamovida;
+								switch(accio) {
+									case 1: 
+										filamovida2--;
+										columnamovida2++;
+										break;
+									case 2: 
+										filamovida2--;
+										columnamovida2--;
+										System.out.println(filamovida2 + " " + columnamovida2);
+										break;
+									case 3: 
+										filamovida2--;
+										columnamovida2--;
+										System.out.println(filamovida2 + " " + columnamovida2);
+										break;
+									default:
+										accio=666;
+										System.out.println("Opcio no valida, torna a introduir:");
+										break;
+								}
+								if(((filamovida2 <= 7 && filamovida2 >= 0) && (columnamovida2 <= 7 && columnamovida2 >= 0)) && tablero[filamovida2][columnamovida2] == "███"){									//comer
+									tablero[filamovida][columnamovida] = "███";
+									tablero[fila][columnaparse] = "███";
+									tablero[filamovida2][columnamovida2] = "█◙█";
+									return 2;
+									System.out.println("return: " + retorn);
+
+								}else{
+									accio=666;
+									System.out.println("Opcio no valida, torna a introduir:");
+									return 1;
+								}
+
+
+
+							}else{
+								tablero[filamovida][columnamovida] = "█◙█";
+								tablero[fila][columnaparse] = "███";
+								return 2;
+							}
+												
+						}else{
+							accio=666;
+							System.out.println("Opcio no valida, torna a introduir:");
+							return 1;
+						}
+					}else{
+						if (filamovida==666 && columnamovida==666){
+							return 1;
+						}else{
+							accio=666;
+							System.out.println("Opcio no valida, torna a introduir:");
+							return 1;
+						}
+
+					}
+
+				
+
+			}else{
+				System.out.println("Posicio incorrecta, torna a escollir:");
+				return 1;
+			}
+		}
+		}while(accio==666);
+		return 1;
+	}
+
+
+
+
+
+
+	static int seleccionarfitxab(String tablero[][], int fila, String columna ) {
+		
+		int columnaparse;
+		int accio = 0;
+		Scanner teclado = new Scanner(System.in);
+
+		switch(columna) {
+			case "A": columnaparse=0;
+				break;
+			case "B": columnaparse=1;
+				break;
+			case "C": columnaparse=2;
+				break;
+			case "D": columnaparse=3;
+				break;
+			case "E": columnaparse=4;
+				break;
+			case "F": columnaparse=5;
+				break;
+			case "G": columnaparse=6;
+				break;
+			case "H": columnaparse=7;
+				break;
+			default:
+				System.out.println("Posicio incorrecta, torna a escollir:");
+				columnaparse=666;
+		}
+
+		int filamovida = fila;
+		int columnamovida = columnaparse;
+		do{
+		if (columnaparse != 666 && (fila <= 7 && fila >= 0)){
+			if(tablero[fila][columnaparse]=="█◘█"){
+
+				
+					filamovida=fila;
+					columnamovida = columnaparse;
+				System.out.println("Cap a on vols avançar?");
+				System.out.println("╔═══════════════════════╗");
+				System.out.println("║1. Avançar dreta       ║");
+				System.out.println("║2. Avançar esquerra    ║");				
+				System.out.println("║3. Enrere              ║");
+				System.out.println("╚═══════════════════════╝");
+				
+				accio = teclado.nextInt();
+
+				switch(accio) {
+					case 1: 
+						filamovida++;
+						columnamovida++;
+						break;
+					case 2: 
+						filamovida++;
+						columnamovida--;
+						break;
+					case 3:
+						filamovida = 666;
+						columnamovida = 666;
+					default:
+						accio=666;
+						break;
+
+					}
+
+
+					if((filamovida <= 7 && filamovida >= 0) && (columnamovida <= 7 && columnamovida >= 0)){
+						if (tablero[filamovida][columnamovida] != "█◘█"){
+							if(tablero[filamovida][columnamovida] == "█◙█"){
+								int filamovida2 = filamovida;
+								int columnamovida2 = columnamovida;
+								switch(accio) {
+									case 1: 
+										filamovida2++;
+										columnamovida2++;
+										break;
+									case 2: 
+										filamovida2++;
+										columnamovida2--;
+										System.out.println(filamovida2 + " " + columnamovida2);
+										break;
+									case 3: 
+										filamovida2++;
+										columnamovida2--;
+										System.out.println(filamovida2 + " " + columnamovida2);
+										break;
+									default:
+										accio=666;
+										System.out.println("Opcio no valida, torna a introduir:");
+										break;
+								}
+								if(((filamovida2 <= 7 && filamovida2 >= 0) && (columnamovida2 <= 7 && columnamovida2 >= 0)) && tablero[filamovida2][columnamovida2] == "███"){									//comer
+									tablero[filamovida][columnamovida] = "███";
+									tablero[fila][columnaparse] = "███";
+									tablero[filamovida2][columnamovida2] = "█◘█";
+									return 1;
+
+								}else{
+									accio=666;
+									System.out.println("Opcio no valida, torna a introduir:");
+									return 2;
+								}
+
+
+
+							}else{
+								tablero[filamovida][columnamovida] = "█◘█";
+								tablero[fila][columnaparse] = "███";
+								return 1;
+							}
+												
+						}else{
+							accio=666;
+							System.out.println("Opcio no valida, torna a introduir:");
+							return 2;
+						}
+					}else{
+						if (filamovida==666 && columnamovida==666){
+							return 2;
+						}else{
+							accio=666;
+							System.out.println("Opcio no valida, torna a introduir:");
+							return 2;
+						}
+
+					}
+
+
+			}else{
+				System.out.println("Posicio incorrecta, torna a escollir:");
+				return 2;
+			}
+		}else{
+			return 2;
+		}
+
+		}while(accio==666);
+
+		
+	}
+
+
 
 	
 	
+
 	public static void main(String[] args) {
 
 		Scanner teclado = new Scanner(System.in);
@@ -91,7 +387,7 @@ public class Dames {
 		
 		//crear matriu
 		omplirTauler(tablero, blanconegre);
-		pintaTauler(tablero);
+		
 		
 		System.out.println();
 		System.out.println("██████╗  █████╗ ███╗   ███╗ █████╗ ███████╗");
@@ -100,6 +396,8 @@ public class Dames {
 		System.out.println("██║  ██║██╔══██║██║╚██╔╝██║██╔══██║╚════██║");
 		System.out.println("██████╔╝██║  ██║██║ ╚═╝ ██║██║  ██║███████║");
 		System.out.println("╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝");
+		System.out.println();
+		pintaTauler(tablero);
 		System.out.println();
 		System.out.println("╔══════════════╗");
 		System.out.println("║Choose:       ║");
@@ -119,12 +417,57 @@ public class Dames {
 			pintaTauler(tablero);
 			
 			double start = (int) (Math.random() * 2) + 1;
-			if(start == 1) {
-				System.out.println("Començen les blanques");
-
+			int torn;
+			if(start == 1 || start == 2) {
+				System.out.println();
+				System.out.println();
+				System.out.println("Començen les negres:");
+				torn=1;
+				System.out.println();
 			}else {
-				System.out.println("Començen les negre");
+				System.out.println();
+				System.out.println();
+				System.out.println("Començen les blanques:");
+				torn=2;
+				System.out.println();
 			}
+
+			do {
+				
+				if(torn == 2){
+
+					System.out.println("Jugador BLANC:");
+					System.out.println("Selecciona la fila de la fitxa que vols moure");
+					int filablanc = teclado.nextInt();
+					filablanc--;
+					System.out.println("Selecciona la columan de la fitxa que vols moure");
+					String columnablanc = teclado.next();
+					torn = seleccionarfitxab(tablero, filablanc, columnablanc );
+					
+								pintaTauler(tablero);
+				}
+				
+
+				if(torn == 1){
+					System.out.println("Jugador NEGRE:");
+					System.out.println("Selecciona la fila de la fitxa que vols moure");
+					int filanegra = teclado.nextInt();
+					filanegra--;
+					System.out.println("Selecciona la columan de la fitxa que vols moure");
+					String columnanegra = teclado.next();
+					torn = seleccionarfitxan(tablero, filanegra, columnanegra );
+					
+								pintaTauler(tablero);
+				}
+
+
+				
+			}while(torn != 3);
+				
+
+				
+				System.out.println("Fi del programa:");
+
 
 			
 		}else if (opcio == 2){
@@ -134,6 +477,5 @@ public class Dames {
 		}
 		
 		
-				
 	}
 }
